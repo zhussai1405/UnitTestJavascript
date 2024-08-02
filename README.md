a2effd7b88e59aa019a20abca45d89be1bbb06d6
zhussai1 <zhussai1@gmail.com>
2024-08-01 21:53:00 -0400

    Added assert for testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UnitTestJavascript
  Intro to Unit Testing
+
+Install Mocha Test Explorer Extension and restart VS Code
--- test/mathTest.js
+++ test/mathTest.js
@@ -1,10 +1,12 @@
+import { assert } from "chai";
 import { add } from "../math.js";
 
 
 describe('Basic Testing Example', function() {
 
     it('1. Testing add functionality', function() {
-         add(3,4);
+         const added = add(3,4);
+         assert.strictEqual(7, added);
     });
 
 
